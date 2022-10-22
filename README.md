1c826602cfd15ae72b58585cc952276b6ac5dc55
Paulo Ferreira <phgferreira@outlook.com>
2022-10-22 13:27:49 -0300

    Iniciado com Spring Boot

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # alura-spring-data-jpa
+
+<div>
+    <img src="readme_img/start_spring_boot_configuration.png" alt="Print de como iniciou a configuração com Spring Boot" />
+</div>
---
+++ src/test/java/br/com/alura/alura/spring/data/jpa/AluraSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.alura.spring.data.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
