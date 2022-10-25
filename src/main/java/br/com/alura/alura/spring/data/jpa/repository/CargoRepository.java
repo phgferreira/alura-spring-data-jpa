187b19779433e15a2a7a435fd34231700ff75d8e
Paulo Ferreira <phgferreira@outlook.com>
2022-10-24 22:46:03 -0300

    Criando um CargoRepository

---
+++ src/main/java/br/com/alura/alura/spring/data/jpa/repository/CargoRepository.java
@@ -0,0 +1,10 @@
+package br.com.alura.alura.spring.data.jpa.repository;
+
+import br.com.alura.alura.spring.data.jpa.orm.Cargo;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CargoRepository extends CrudRepository {
+
+}
