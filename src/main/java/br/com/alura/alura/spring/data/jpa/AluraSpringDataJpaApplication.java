7019d927b3679960f21893759aa412bb4786045a
Paulo Ferreira <phgferreira@outlook.com>
2022-10-24 22:56:51 -0300

    Fazendo um teste de inserção de cargo no banco de dados

--- src/main/java/br/com/alura/alura/spring/data/jpa/AluraSpringDataJpaApplication.java
+++ src/main/java/br/com/alura/alura/spring/data/jpa/AluraSpringDataJpaApplication.java
@@ -1,13 +1,27 @@
 package br.com.alura.alura.spring.data.jpa;
 
+import br.com.alura.alura.spring.data.jpa.orm.Cargo;
+import br.com.alura.alura.spring.data.jpa.repository.CargoRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AluraSpringDataJpaApplication {
+public class AluraSpringDataJpaApplication implements CommandLineRunner {
+
+	private final CargoRepository repository;
+
+	public AluraSpringDataJpaApplication(CargoRepository repository) {
+		this.repository = repository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AluraSpringDataJpaApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Cargo cargo = new Cargo("Desenvolvedor de Software");
+		repository.save(cargo);
+	}
 }
