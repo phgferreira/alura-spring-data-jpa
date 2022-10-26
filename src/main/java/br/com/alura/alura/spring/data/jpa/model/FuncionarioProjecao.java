e0545311eb5b2cffaaa7a437be35cc9540af563d
Paulo Ferreira <phgferreira@outlook.com>
2022-10-25 22:43:21 -0300

    Usando projeção de entidades para otimizar as consultas

---
+++ src/main/java/br/com/alura/alura/spring/data/jpa/model/FuncionarioProjecao.java
@@ -0,0 +1,11 @@
+package br.com.alura.alura.spring.data.jpa.model;
+
+import java.math.BigDecimal;
+
+public interface FuncionarioProjecao {
+
+    Integer getId();
+    String getNome();
+    BigDecimal getSalario();
+
+}
--- src/main/java/br/com/alura/alura/spring/data/jpa/repository/FuncionarioRepository.java
+++ src/main/java/br/com/alura/alura/spring/data/jpa/repository/FuncionarioRepository.java
@@ -1,21 +1,25 @@
 package br.com.alura.alura.spring.data.jpa.repository;
 
 import br.com.alura.alura.spring.data.jpa.model.Funcionario;
+import br.com.alura.alura.spring.data.jpa.model.FuncionarioProjecao;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
-public interface FuncionarioRepository extends CrudRepository<Funcionario, Integer> {
+    public interface FuncionarioRepository extends PagingAndSortingRepository<Funcionario, Integer> {
 
-    List<Funcionario> findByNome(String nome);
+    List<Funcionario> findByNome(String nome, Pageable pageable);
 
     @Query("select f from Funcionario f where f.nome = :nome and f.salario >= :salario and f.dataContratacao = :dataContratacao")
-    List<Funcionario> findByNomeAndSalario(String nome, BigDecimal salario, LocalDate dataContratacao);
+    List<Funcionario> findByNomeAndSalario(String nome, BigDecimal salario, LocalDate dataContratacao, Pageable pageable);
 
     @Query(nativeQuery = true, value="select * from funcionario f where f.data_contratacao >= :data")
     List<Funcionario> findByDataContratacaoMaior(LocalDate data);
+
+    @Query(nativeQuery = true, value = "select f.id, f.nome, f.salario from funcionario f")
+    List<FuncionarioProjecao> findFuncionarioSalario();
 }
