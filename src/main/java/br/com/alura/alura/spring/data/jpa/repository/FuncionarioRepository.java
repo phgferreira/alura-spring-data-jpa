7d65cae0b6b5a8059a948310d25d2b0750dca5d2
Paulo Ferreira <phgferreira@outlook.com>
2022-10-25 21:14:22 -0300

    Criando um método no repository do Funcionario listando por nome

--- src/main/java/br/com/alura/alura/spring/data/jpa/repository/FuncionarioRepository.java
+++ src/main/java/br/com/alura/alura/spring/data/jpa/repository/FuncionarioRepository.java
@@ -3,5 +3,10 @@ package br.com.alura.alura.spring.data.jpa.repository;
 import br.com.alura.alura.spring.data.jpa.model.Funcionario;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface FuncionarioRepository extends CrudRepository<Funcionario, Integer> {
+
+    List<Funcionario> findByNome(String nome);
+
 }
